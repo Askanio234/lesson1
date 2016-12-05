bbbfc699e6d3d25d9d36f60e2dd00303a9722a24
Artem Chernyaev <askanio234@gmail.com>
2016-12-05 23:48:54 +0300

    Expanded bot functionality, added new commands and response via dict

--- answers.py
+++ answers.py
@@ -1,6 +1,11 @@
+phrasedict = {"привет":"И тебе привет!", "как дела?":"Лучше всех", "пока":"Увидимся"}
+def get_answer(key, dict):
+	if key.lower() in dict:
+		return dict.get(key.lower())
+	else:
+		return "Я не понял, попробуй другую фразу..."
+
 if __name__ == '__main__':
-	phrasedict = {"привет":"И тебе привет!", "как дела":"Лучше всех", "пока":"Увидимся"}
-	def get_answer(key, dict):
-		print(dict.get(key.lower()))
+	
 	user_choice = input("Введите фразу: ")
 	get_answer(user_choice,phrasedict)
--- bot.py
+++ bot.py
@@ -1,23 +1,41 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from answers import phrasedict, get_answer
 
 def talk_to_me(bot, update):
     print('Пришло сообщение: {}'.format(update.message.text))
-    bot.sendMessage(update.message.chat_id, update.message.text)
+    bot.sendMessage(update.message.chat_id, get_answer(update.message.text, phrasedict))
 
 def show_error(bot, update, error):
     print('Update "{}" caused error "{}"'.format(update, error))
 
-def greet_user(bot,update):
-	print("Вызван /старт")
-	bot.sendMessage(update.message.chat_id, text='Давай общаться!')
+def help(bot, update):
+	print("Вызван /помощь")
+	bot.sendMessage(update.message.chat_id, text='/start \n/killallhumans \n/showmeyourcat')
 	print(update.message)
 
+def greet_user(bot,update):
+	print("Вызван /старт")
+	bot.sendMessage(update.message.chat_id, text='Давай общаться! Не знаешь что написать? попробуй /help')
+	print(update.message)
+
+def show_me_your_cat(bot, update):
+	print('Вызван /showmeyourcat')
+	bot.sendPhoto(update.message.chat_id, photo='https://pp.vk.me/c303300/v303300996/43f1/kxMJre_gSrY.jpg')
+	print(update.message)
+
+def kill_allhumans(bot, update):
+	print("Вызван /убитьвсехчеловеков")
+	bot.sendMessage(update.message.chat_id, text='Убить всех человеков через 3...2...1..')
+	print(update.message)
 
 def main():
 	updater = Updater("326458218:AAEKVLRflUisJUNY5JijTnTObHeacdPBQsQ")
 
 	dp = updater.dispatcher
-	dp.add_handler(CommandHandler("start", greet_user))
+	dp.add_handler(CommandHandler('start', greet_user))
+	dp.add_handler(CommandHandler('killallhumans', kill_allhumans))
+	dp.add_handler(CommandHandler('help', help))
+	dp.add_handler(CommandHandler('showmeyourcat', show_me_your_cat))
 
 	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
